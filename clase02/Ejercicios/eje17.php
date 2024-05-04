5b57265f221ed77505afbde0a964a3387d64a600
villalbapaezmiguel <villalbapaezmiguel@gmai.com>
2024-05-04 02:26:22 -0300

    haciendo ejer 17

--- clase02/Ejercicios/eje17.php
+++ clase02/Ejercicios/eje17.php
@@ -1,11 +1,13 @@
 <?php
+
 /** */
-class Auto{
+class Auto
+{
 
     //atributos: color , precio , marca y fecha
-    private $color ;
-    private $precio ;
-    private $marca ;
+    private $color;
+    private $precio;
+    private $marca;
     private DateTime $fecha;
 
     public function __construct()
@@ -16,24 +18,144 @@ class Auto{
         //obtenemos la cantidad de parametros pasados por el constructor
         $cantidadParametros = func_num_args();
 
-        //
-
+        //obtenemos el nombre de la funcion
+        $nombreContructor = '__construc' . $cantidadParametros;
+        if (method_exists($this, $nombreContructor)) {
+            //si existe , llamo al contructor que tenga ese numero de parametros
+            call_user_func_array(array($this, $nombreContructor), $arrayParametros);
+        }
     }
 
-    function __construct0($marca , $color)
+
+    function __construct0($marca, $color, $precio = 0)
+    {
+        echo "<br> valor de la marca : $marca <br>";
+        $this->SetMarca($marca);
+        $this->color = $color;
+        $this->precio = $precio;
+        $this->fecha = new DateTime();
+    }
+
+    function __construct1($marca, $color, $precio)
     {
         $this->marca = $marca;
         $this->color = $color;
+        $this->precio = $precio;
+        $this->fecha = new DateTime();
+    }
+
+    function __construct2($marca, $color, $precio, $fecha)
+    {
+        $this->marca = $marca;
+        $this->color = $color;
+        $this->precio = $precio;
+        $this->fecha = $fecha;
+    }
+    /*
+    //marca y color
+    function __construct0($marca , $color, $precio = 0 )
+    {
+        $this->marca = $marca;
+        $this->color = $color;
+        $this->precio = $precio;
+        $this->fecha = new DateTime();
     }
 
 
 
+    //marca , color y precio
+    function __construct1($marca , $color , $precio)
+    {
+        $this->marca = $marca;
+        $this->color = $color;
+        $this->precio = $precio;
+        $this->fecha = new DateTime();
+    }
+
+    // La marca, color, precio y fecha.
+    function __construct2($marca , $color , $precio , $fecha)
+    {
+        $this->marca = $marca;
+        $this->color = $color;
+        $this->precio = $precio;
+        $this->fecha = $fecha;
+        
+    }*/
 
 
+    //generamos getters
+
+    public function GetMarca()
+    {
+        return $this->marca;
+    }
+
+    public function GetColor()
+    {
+        return $this->color;
+    }
+
+    public function GetPrecio()
+    {
+        return $this->precio;
+    }
+
+    public function GetFecha()
+    {
+        return $this->fecha ?? null;
+    }
 
 
+    //generamos setters
+
+    public function SetMarca($marca)
+    {
+        $this->marca = $marca;
+    }
+
+    public function SetColor($color)
+    {
+        $this->color = $color;
+    }
+
+    public function SetPrecio($precio)
+    {
+        $this->precio = $precio;
+    }
+
+    public function SetFecha($fecha)
+    {
+        $this->fecha = $fecha;
+    }
+
+    //Funciones 
+
+    public function AgregarImpuestos($impuesto)
+    {
+        // sumará al precio del objeto
+        $this->precio += $impuesto;
+    }
+    public static function MostrarAuto($auto)
+    {
+        $auxMarca = $auto->GetMarca();
+        $auxColor = $auto->GetColor();
+        $auxPrecio = $auto->GetPrecio();
+        $auxFecha = $auto->GetFecha() ?? "No especificada";
+
+        $infoAuto = "Marca: " . ($auxMarca ?? "No especificada") . "<br>" .
+            "Color: " . ($auxColor ?? "No especificada") . "<br>" .
+            "Precio: " . ($auxPrecio ?? "No especificada") . "<br>" .
+            "Fecha: " . $auxFecha . "<br>";
+
+        echo $infoAuto;
+    }
 }
 
+echo "hola migue, sos un gran programador!! <br>";
 
+$auto1 = new Auto("Fiat", "rojo");
+$auto2 = new Auto("Fiat", "rojo");
 
-?>
+Auto::MostrarAuto($auto1);
+echo "<br>";
+Auto::MostrarAuto($auto2);
